045873708c5ce4ce2bd5b1b216e65bdfbd05075d
omar tarik yehya <khalaf.omar20@gmail.com>
2024-11-11 16:43:31 +0200

    feat: rendering expenses list

--- components/ExpensesOutput/ExpensesOutput.jsx
+++ components/ExpensesOutput/ExpensesOutput.jsx
@@ -2,13 +2,33 @@ import { View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
-const DUMMY_EXPENSES = 
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        description: 'A pair of shoes',
+        amount: 59.99,
+        date: new Date('2021-12-19')
+    },
+    {
+        id: 'e2',
+        description: 'A pair of headphones',
+        amount: 89.99,
+        date: new Date('2021-12-30')
+    },
+    {
+        id: 'e3',
+        description: 'A Trousser',
+        amount: 56.99,
+        date: new Date('2021-12-18')
+    }
+
+]
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View>
-      <ExpensesSummary expenses={expenses} periodName={expensesPeriod}/>
-      <ExpensesList />
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod}/>
+      <ExpensesList  expenses={DUMMY_EXPENSES}/>
     </View>
   );
 }
