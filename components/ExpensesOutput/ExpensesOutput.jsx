711eef9a49cc4ff421e2efae8c36666bbfdcd16c
omar tarik yehya <khalaf.omar20@gmail.com>
2024-11-11 16:16:17 +0200

    feat: added bottom nav

---
+++ components/ExpensesOutput/ExpensesOutput.jsx
@@ -0,0 +1,16 @@
+import { View } from "react-native";
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+
+const DUMMY_EXPENSES = 
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
+  return (
+    <View>
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod}/>
+      <ExpensesList />
+    </View>
+  );
+}
+
+export default ExpensesOutput;
