13b135fe098ce2f7cc4a3656f7fb4d70490e1c51
omar tarik yehya <khalaf.omar20@gmail.com>
2024-11-13 13:07:08 +0200

    feat: added custom buttons

--- screens/ManageExpenses.jsx
+++ screens/ManageExpenses.jsx
@@ -2,6 +2,7 @@ import { View, StyleSheet } from "react-native";
 import { useLayoutEffect } from "react";
 import IconButton from "../UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
+import Button from "../UI/Button";
 
 const ManageExpenses = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
@@ -14,10 +15,19 @@ const ManageExpenses = ({ route, navigation }) => {
   }, [navigation, isEditing]);
 
   function deleteExpenseHandler() {}
+  function cancelHandler() {}
+  function confirmHandler() {}
 
   return (
-    <View style={styles.container
-    }>
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -38,13 +48,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
-    backgroundColor: GlobalStyles.colors.primary800
+    backgroundColor: GlobalStyles.colors.primary800,
   },
   deleteContainer: {
-    marginTop:16,
+    marginTop: 16,
     paddingTop: 8,
     borderTopWidth: 2,
     borderTopColor: GlobalStyles.colors.primary200,
-    alignItems: 'center'
-  }
+    alignItems: "center",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
 });
